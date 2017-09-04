3318fc553b3913156a2f6c1345826c031f79799c
Aditya Thakral <aditya.1998thakral@gmail.com>
2017-09-05 03:18:31 +0530

    Hello World client

---
+++ client/app/404/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = props => {
+  return (
+    <div>
+      <h1>404: Page Not Found</h1>
+      <p>This is not the page you're looking for</p>
+    </div>
+  )
+}
+
+export default NotFound
